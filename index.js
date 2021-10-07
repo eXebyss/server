4ca88e59e5c27c50710b08263b91db368d8b8a34
eXebyss <mysticlord27@gmail.com>
2021-10-07 23:22:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const authRouter = require('./routes/auth.routes')
+const cors = require('cors')
+
+const PORT = process.env.PORT || config.get('serverPort')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth', authRouter)
+
+const start = async () => {
+	try {
+		await mongoose.connect(config.get('dbUrl'))
+
+		app.listen(PORT, () => {
+			console.log('Server listening on port: ', PORT)
+		})
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+start()
