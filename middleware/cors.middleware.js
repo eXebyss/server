4d16a2f6265fb39481928f0320e191f09e5b316b
eXebyss <mysticlord27@gmail.com>
2021-11-02 14:18:48 +0200

    cors fix2

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Origin', '*, https://mern-app-eight.vercel.app/')
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE')
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 	next()
